4edcb2e2775951525273a2b6543ac4150bc63c17
Razor <alexandergray37@gmail.com>
2020-10-05 19:09:47 -0400

    created auction contract

---
+++ token/contracts/Auction.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.7.0;
+
+contract Auction {
+
+    address payable public beneficiary;
+    address public highestBidder;
+    uint public HighestBid;
+    uint public auctionEndtime; 
+
+}
